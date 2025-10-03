c29067cd7cd2d0bd781dcb4c22595522db5a443f
amonsho <amonsho004@gmail.com>
2025-10-03 22:28:04 +0500

    pagination

--- Bookings/views.py
+++ Bookings/views.py
@@ -3,10 +3,12 @@ from .models import Bookings
 from .serializers import BookingSerializer
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsUserOrAdmin, IsOwnerOrAdmin
+from .pagination import BookingPagination
 
 class BookingViewset(viewsets.ModelViewSet):
     queryset = Bookings.objects.all()
     serializer_class = BookingSerializer
+    pagination_class = BookingPagination
     permission_classes = [IsAuthenticated, IsUserOrAdmin, IsOwnerOrAdmin]
 
     def perform_create(self, serializer):
